76ffc43c2aa5b082b302652275ac471b21c0aab1
庞云川 <pangyunchuan@bonc.com.cn>
2023-04-28 17:20:37 +0800

    修改

--- template-vite-vue3-setup-ts/env.d.ts
+++ template-vite-vue3-setup-ts/env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="vite/client" />
-
-/// <reference types="@types/baidumap-web-sdk" />
-
-
-import {DefineComponent} from "vue";
-
-declare module '*.vue' {
-    import { DefineComponent } from 'vue'
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-    const component: DefineComponent<{}, {}, any>
-    export default component
-}
